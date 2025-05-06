6d51eb0a68146e87f202ef16b510d73989652111
Camilo <camilo.santivanez@ug.uchile.cl>
2025-05-06 16:53:08 -0400

    feat:implementing references to every html in flask format

--- app.py
+++ app.py
@@ -3,9 +3,20 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('html/index.html')
 
+@app.route('/listado')
+def listado():
+    return render_template('html/listado.html')
+
+@app.route('/estadisticas')
+def estadisticas():
+    return render_template('html/estadisticas.html')
+
+@app.route('/agregar_actividad')
+def agregar():
+    return render_template('html/agregar_actividad.html')
 if __name__ == '__main__':
     app.run(debug=True)
 
