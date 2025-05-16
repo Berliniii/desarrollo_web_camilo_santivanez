a17c68c76c4b411df8c64ac3aa5f83692d488ff8
Camilo <camilo.santivanez@ug.uchile.cl>
2025-05-16 19:16:05 -0400

    feat(social.js): adding subirFoto function to upload 1 up to 5 photos

--- static/js/socials.js
+++ static/js/socials.js
@@ -6,3 +6,14 @@ function revisaCheck(element){
         document.getElementById(element.name).style.display = "none";
     }
 }
+
+function subirFoto(element) {
+    // Obtener el número actual de la foto (último carácter del id)
+    const currentNum = parseInt(element.id.slice(-1));
+    
+    // Dar la opcion de ingresar otra foto
+    const nextInput = document.getElementById(`foto${currentNum + 1}`);
+    if (nextInput) {
+        nextInput.style.display = "block";
+    }
+}
